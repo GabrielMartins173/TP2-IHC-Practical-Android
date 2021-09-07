691aec1ac3211f0d6bf508113d1746640d5048a5
Gabriel Martins <gabrielmartins173@gmail.com>
2021-09-07 19:13:00 -0300

    finished activity 4

--- app/src/main/java/com/example/tp2_ihc/Activity4.java
+++ app/src/main/java/com/example/tp2_ihc/Activity4.java
@@ -2,13 +2,49 @@ package com.example.tp2_ihc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class Activity4 extends AppCompatActivity {
+public class Activity4 extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor light;
+    TextView lightValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4);
+
+        lightValue = (TextView) findViewById(R.id.light);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        if(light != null){
+            sensorManager.registerListener(Activity4.this, light, SensorManager.SENSOR_DELAY_NORMAL);
+        }else{
+            lightValue.setText("Light sensor not supported.");
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor sensor = sensorEvent.sensor;
+
+        if(sensor.getType() == Sensor.TYPE_LIGHT){
+            lightValue.setText("Light Intensity:" + sensorEvent.values[0]);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
