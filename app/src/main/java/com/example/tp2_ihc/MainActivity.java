8a50f46c7438172b34804061b337e63d2e88e5b4
Gabriel Martins <gabrielmartins173@gmail.com>
2021-09-07 15:57:25 -0300

    finishing the menu of the application

--- app/src/main/java/com/example/tp2_ihc/MainActivity.java
+++ app/src/main/java/com/example/tp2_ihc/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.tp2_ihc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void StartActivity1(View view){
+
+        Intent i = new Intent(this,Activity1.class);
+        startActivity(i);
+
+    }
+
+    public void StartActivity2(View view){
+
+        Intent i = new Intent(this,Activity2.class);
+        startActivity(i);
+
+    }
+
+    public void StartActivity3(View view){
+
+        Intent i = new Intent(this,Activity3.class);
+        startActivity(i);
+
+    }
+
+    public void StartActivity4(View view){
+
+        Intent i = new Intent(this,Activity4.class);
+        startActivity(i);
+
+    }
+
+    public void StartActivity5(View view){
+
+        Intent i = new Intent(this,Activity5.class);
+        startActivity(i);
+
+    }
+
+    public void StartActivity6(View view){
+
+        Intent i = new Intent(this,Activity6.class);
+        startActivity(i);
+
     }
 }
